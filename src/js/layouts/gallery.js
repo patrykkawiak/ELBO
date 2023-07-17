8c7b78b1a48829112341d89cf67086ed8c6bca01
Mateusz Tryc <mateusz.tryc420@gmail.com>
2023-07-17 12:47:45 +0200

    made the gallery working

---
+++ src/js/layouts/gallery.js
@@ -0,0 +1,183 @@
+const prev = document.querySelector('.button.prev')
+const next = document.querySelector('.button.next')
+// const sliderTrack = document.querySelector('.slider-track')
+const galleryImgBoxes = document.querySelectorAll('.slider-img-box')
+
+// let index = 1
+// const maxIndex = galleryImgs.length - 1
+
+// const galleryImgWidth = galleryImgs[0].clientWidth
+// console.log(galleryImgWidth * index);
+
+// const handleGallery = () => {
+//   sliderTrack.style.transform = `translateX(Calc(-${galleryImgWidth * index}px + ${galleryImgWidth / 2}px))`
+//   galleryImgs.forEach(img => {
+//     img.classList.remove('active')
+//   })
+//   galleryImgs[index].classList.add('active')
+// }
+
+// prev.addEventListener('click', () => {
+//   if(index === 2){
+//     const prevImg = galleryImgs[maxIndex].cloneNode(true)
+//   }
+//   index--
+//   handleGallery()
+// })
+// next.addEventListener('click', () => {
+//   index++
+//   handleGallery()
+// })
+
+// handleGallery()
+
+const getActiveImg = () => document.querySelector('.slider-img-box.active')
+const getPrevImg = () => document.querySelector('.slider-img-box.prev')
+const getNextImg = () => document.querySelector('.slider-img-box.next')
+const getNexterImg = () => document.querySelector('.slider-img-box.nexter')
+const getPreviouserImg = () => document.querySelector('.slider-img-box.previouser')
+let index = 3
+const maxIndex = galleryImgBoxes.length - 1
+let isTransitioning = false
+
+const handleNewClassAssign = ({element, toRemove, toAdd}) => {
+  if(toAdd){
+    if(typeof toAdd === typeof []){
+      toAdd.forEach(className => {
+        element.classList.add(className)
+      })
+    }
+    else {
+      element.classList.add(toAdd)
+    }
+  }
+  if(toRemove){
+    if(typeof toRemove === typeof []){
+      toRemove.forEach(className => {
+        element.classList.remove(className)
+      })
+    }
+    else {
+      element.classList.remove(toRemove)
+    }
+  }
+}
+const handlePrev = () => {
+  if(!isTransitioning){
+    isTransitioning = true
+    let newPreviouserIndex = index - 3
+    if(newPreviouserIndex < 0){
+      newPreviouserIndex = maxIndex + newPreviouserIndex + 1
+    }
+    const previouser = getPreviouserImg()
+    const nexter = getNexterImg()
+    const active = getActiveImg()
+    let prev = getPrevImg()
+    let next = getNextImg()
+    handleNewClassAssign({
+      element: prev,
+      toAdd: 'active',
+      toRemove: 'prev'
+    })
+    handleNewClassAssign({
+      element: active,
+      toAdd: 'next',
+      toRemove: 'active'
+    })
+    handleNewClassAssign({
+      element: next,
+      toAdd: 'nexter',
+      toRemove: 'next'
+    })
+    handleNewClassAssign({
+      element: nexter,
+      toRemove: ['nexter', 'shown']
+    })
+    handleNewClassAssign({
+      element: previouser,
+      toRemove: 'previouser',
+      toAdd: 'prev'
+    })
+    const newPreviouser = galleryImgBoxes[newPreviouserIndex]
+    handleNewClassAssign({
+      element: newPreviouser,
+      toAdd: 'previouser'
+    })
+    prev = getPrevImg()
+    next = getNextImg()
+    prev.onclick = () => handlePrev()
+    next.onclick = () => handleNext()
+    setTimeout(() => {
+      newPreviouser.classList.add('shown')
+      isTransitioning = false
+    }, 700)
+    index--
+    if(index < 0){
+      index = maxIndex
+    }
+  }
+}
+const handleNext = () => {
+  if(!isTransitioning){
+    isTransitioning = true
+    let newNexterIndex = index + 3
+    if(newNexterIndex > maxIndex){
+      newNexterIndex = newNexterIndex - maxIndex - 1
+    }
+    const previouser = getPreviouserImg()
+    const nexter = getNexterImg()
+    const active = getActiveImg()
+    let prev = getPrevImg()
+    let next = getNextImg()
+    handleNewClassAssign({
+      element: prev,
+      toAdd: 'previouser',
+      toRemove: 'prev'
+    })
+    handleNewClassAssign({
+      element: active,
+      toAdd: 'prev',
+      toRemove: 'active'
+    })
+    handleNewClassAssign({
+      element: next,
+      toAdd: 'active',
+      toRemove: 'next'
+    })
+    handleNewClassAssign({
+      element: nexter,
+      toAdd: 'next',
+      toRemove: 'nexter'
+    })
+    handleNewClassAssign({
+      element: previouser,
+      toRemove: ['previouser', 'shown']
+    })
+    const newNexter = galleryImgBoxes[newNexterIndex]
+    handleNewClassAssign({
+      element: newNexter,
+      toAdd: 'nexter'
+    })
+    index++
+    if(index > maxIndex){
+      index = 0
+    }
+    prev = getPrevImg()
+    next = getNextImg()
+    prev.onclick = () => handlePrev()
+    next.onclick = () => handleNext()
+    setTimeout(() => {
+      newNexter.classList.add('shown')
+      isTransitioning = false
+    }, 700)
+  }
+}
+prev.addEventListener('click', handlePrev)
+next.addEventListener('click', handleNext)
+
+window.addEventListener('DOMContentLoaded', () => {
+  const prevImg = getPrevImg()
+  const nextImg = getNextImg()
+  prevImg.onclick = () => handlePrev()
+  nextImg.onclick = () => handleNext()
+})
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './layouts/nav.js'
+import './layouts/gallery.js'
 import { handlePrallax } from './layouts/breaker.js';
 AOS.init();
 
